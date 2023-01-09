524a1ef3a37b08fcea9b5e5f4315df338383fa0b
Danyspb4ever <danyspb4ever@gmail.com>
2023-01-09 22:44:57 +0000

    add screen page

---
+++ Screens/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+
+
+
+const Home = () =>{
+    return(
+        <View>
+            <Text>
+                Welcome to my home page
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+
+})
+
+export default Home;
---
+++ Screens/Profil.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+
+
+const Profil = () =>{
+    return(
+        <View>
+            <Text>
+                Welcome to my profil page
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Profil;
