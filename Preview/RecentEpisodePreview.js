f5fcdba2ef32959cb3dba0ee9a9ad47e5285ee64
Danyspb4ever <danyspb4ever@gmail.com>
2023-06-15 17:31:52 +0000

    Style: styling the  new preview

--- Preview/RecentEpisodePreview.js
+++ Preview/RecentEpisodePreview.js
@@ -26,4 +26,46 @@ const RecentEpisodePreview = ()=>{
     )
 
 }
+const styles = StyleSheet.create({
+    card:{
+        backgroundColor: 'pink',
+        width: 125,
+        height: 170,
+        borderColor: 'yellow',
+        borderRadius: 10,
+        borderWidth: 1,
+        margin: 6,
+        marginBottom: 50
+    },
+    cardPic:{
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        borderRadius: 11,
+
+    },
+    titre:{
+        color: 'white',
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    labelAnime: {
+        color: 'white',
+        marginTop: -30,
+        marginBottom: 15,
+        paddingLeft: 20,
+        fontWeight: 'bold',
+        fontSize: 25,
+    },
+    textLabel:{
+        color: 'white',
+        marginTop: -30,
+        marginBottom: 15,
+        paddingLeft: 20,
+        fontWeight: 'bold',
+        fontSize: 25,
+
+    }
+})
 export default RecentEpisodePreview;
