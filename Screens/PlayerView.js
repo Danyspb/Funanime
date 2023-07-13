3315b906e159c3a0cfeaa865de1cd280f8946fe2
Danyspb4ever <danyspb4ever@gmail.com>
2023-07-13 22:58:35 +0000

    feat: player work but need styling

--- Screens/PlayerView.js
+++ Screens/PlayerView.js
@@ -1,6 +1,7 @@
+import { ResizeMode, Video } from "expo-av";
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
-import NexenPlayer, { NexenPlayerRef } from 'react-native-nexen-player';
+import { View, Text, StyleSheet, ActivityIndicator, Button } from "react-native";
+
 
 
 const PlayerView = ({route})=>{
@@ -8,7 +9,8 @@ const PlayerView = ({route})=>{
     let serverID = route.params;
     let [link, setLink] = useState([]);
     let [loading, setLoading] = useState([]);
-    l
+    const video = React.useRef(null);
+    const [status, setStatus] = React.useState({})
 
 
     const url = `https://api.consumet.org/anime/gogoanime/watch/${serverID}`;
@@ -24,17 +26,6 @@ const PlayerView = ({route})=>{
         )
     },[]);
 
-   
-
-    {
-        link.sources?.map((item)=>{
-            return(
-                console.log(item.url),
-                console.log(item.quality)
-            )
-        })
-    }
-
     if(loading){
         return<View style={styles.activityStyle}>
             <ActivityIndicator size={"large"} color={"#00ff00"} />
@@ -42,23 +33,54 @@ const PlayerView = ({route})=>{
     }
 
     return(
-        <View style={styles.videoContainer}>
-           <Text> hello to the  player screen</Text>
+        <View style={styles.container}>  
+           {
+             link.sources?.map((item, index)=>{
+                return(
+                    <View>
+                        <Video 
+                        style={styles.videoStyle}
+                        key={index}
+                        ref={video}
+                        source={{uri: item.url}}
+                        useNativeControls
+                        resizeMode={ResizeMode.CONTAIN}
+                        isLooping={false}
+                        onPlaybackStatusUpdate={status => setStatus(() => status)}
+                    /> 
+                    </View>
+                    
+                )
+             })
+           }
+           <View>
+                <Button 
+                    title={status.isPlaying ? 'Pause' : 'Play'}
+                    onPress={()=>
+                        status.isPlaying ?  video.current.pauseAsync() : video.current.playAsync()
+                    }
+                /> 
+           </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+
+    container:{
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: "flex-end",
+
+    },
     activityStyle:{
         flex: 1,
         justifyContent: "center",
         alignContent: "center",
         backgroundColor: '#181D31',
     },
-    videoContainer:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: "center",
+    videoStyle:{
+        backgroundColor: 'red'
 
     }
 })
