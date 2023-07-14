641ed9328d486436660211071ff57d154f30bcea
Danyspb4ever <danyspb4ever@gmail.com>
2023-07-14 21:24:50 +0000

    style: styling the player

--- Screens/PlayerView.js
+++ Screens/PlayerView.js
@@ -1,6 +1,7 @@
 import { ResizeMode, Video } from "expo-av";
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, ActivityIndicator, Button } from "react-native";
+import { View,Text, StyleSheet, ActivityIndicator,ScrollView, Button, SafeAreaView, TouchableOpacity, Modal, Dimensions } from "react-native";
+
 
 
 
@@ -9,9 +10,8 @@ const PlayerView = ({route})=>{
     let serverID = route.params;
     let [link, setLink] = useState([]);
     let [loading, setLoading] = useState([]);
-    const video = React.useRef(null);
-    const [status, setStatus] = React.useState({})
-
+    let video = React.useRef(null);
+    let [status, setStatus] = React.useState({});
 
     const url = `https://api.consumet.org/anime/gogoanime/watch/${serverID}`;
 
@@ -26,6 +26,9 @@ const PlayerView = ({route})=>{
         )
     },[]);
 
+
+    
+
     if(loading){
         return<View style={styles.activityStyle}>
             <ActivityIndicator size={"large"} color={"#00ff00"} />
@@ -33,28 +36,27 @@ const PlayerView = ({route})=>{
     }
 
     return(
-        <View style={styles.container}>  
-        
-           {
-            link.sources?.map((item,index)=>{
-                return<Video key={index}
-                    ref={video}
-                    style={styles.video}
-                    source={{
-                        uri: item.url
-                    }}
-                    useNativeControls
-                    resizeMode={ResizeMode.CONTAIN}
-                    isLooping={true}
-                    onPlaybackStatusUpdate={status => setStatus(() => status)}
-                />
-            })
-           }
+        <View style={styles.container}>
+                {
+                    link.sources?.map((item,index)=>{
+                        return<Video 
+                                key={index}
+                                ref={video}
+                                style={styles.video}
+                                source={{uri: item.url}}
+                                useNativeControls
+                                resizeMode={ResizeMode.COVER}
+                                isLooping
+                                onPlaybackStatusUpdate={status => setStatus(() => status)}
+                        />
+                    })
+                }
+            
            <View>
                 <Button 
                     title={status.isPlaying ? 'Pause' : 'Play'}
                     onPress={()=>
-                        status.isPlaying ?  video.current.pauseAsync(3000) : video.current.playAsync()
+                        status.isPlaying ?  video.current.pauseAsync() : video.current.playAsync()
                     }
                 /> 
            </View>
@@ -65,13 +67,12 @@ const PlayerView = ({route})=>{
 const styles = StyleSheet.create({
 
     container:{
-        flex: 1,
-        alignContent: 'center',
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: 'blue'
+        flex: 3,
+
+        backgroundColor: '#181D31'
 
     },
+   
     activityStyle:{
         flex: 1,
         justifyContent: "center",
@@ -79,13 +80,10 @@ const styles = StyleSheet.create({
         backgroundColor: '#181D31',
     },
     video:{
-       margin: 5,
-       height: "40%",
-       width: '90%',
-       justifyContent: 'center',
-       
-        
-    }
+       flex: 1,
+      
+    },
+
 })
 
 export default PlayerView;
